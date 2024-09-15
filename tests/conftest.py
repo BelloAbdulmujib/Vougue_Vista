d6c91a9fb596ca70630eb3a9c800654bdc348c4e
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-15 02:32:18 +0100

    all basic testing files are set up

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,8 @@
 import pytest
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from app import create_app, db
 from config import TestConfig
 
