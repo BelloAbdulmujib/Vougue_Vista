8c0e0f81348b63be1e916d4233ccae3449723983
Ughagwu Oluchukwu <oluchristian98@gmail.com>
2024-09-15 01:58:18 +0100

    created a conftest file

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,12 +1,30 @@
 import pytest
-from app import create_app
+from app import create_app, db
+from config import TestConfig
 
-@pytest.fixture()
+@pytest.fixture(scope='session')
 def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+    app = create_app(config_class=TestConfig)
+    with app.app_context():
+        db.create_all()
     # other setup
-    yield app
+        yield app
+        db.session.remove()
+        db.drop_all()
     # clean up
+
+@pytest.fixture(scope='session')
+def client(app):
+    return app.test_client()
+
+@pytest.fixture(scope='function')
+def init_database(app):
+    with app.app_context():
+        db.create_all()
+        yield db
+        db.session.remove()
+        db.drop_all()
+
+@pytest.fixture(scope='function')
+def runner(app):
+    return app.test_cli_runner()
