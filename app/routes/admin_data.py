4382d8527e735ee08aacb4b9695ee17f70db35b5
BelloAbdulmujib <142020803+BelloAbdulmujib@users.noreply.github.com>
2024-09-18 14:20:04 -1100

    Update admin_data.py

--- app/routes/admin_data.py
+++ app/routes/admin_data.py
@@ -9,7 +9,7 @@ admin_data_bp = Blueprint('admin_data', __name__)
 
 # Function to connect to the SQLite database
 def get_db_connection():
-    conn = sqlite3.connect('instance/VogueVista.sqlite3')  # Replace with your database file name
+    conn = sqlite3.connect('instance/VogueVista.sqlite3') 
     conn.row_factory = sqlite3.Row  # Optional: allows access to columns by name
     return conn
 
@@ -23,7 +23,7 @@ def insert_data():
     cursor.execute('''
         INSERT INTO admin (username, email, password)
         VALUES (?, ?, ?)
-    ''', ('admin001', 'admin001@gmail.com', '12345678'))  # Replace with actual values
+    ''', ('admin001', 'admin001@gmail.com', '12345678'))
 
     # Commit the changes and close the connection
     conn.commit()
